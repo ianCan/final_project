5d89dc63e6491cb600e1ba08e476bf93af911468
BEASTELMO <beastelmo8@gmail.com>
2021-07-12 17:44:05 -0400

    added lyric finder

--- server.js
+++ server.js
@@ -85,26 +85,27 @@ APP.get('/delete-session', (req, res) => {
     res.redirect('/playlist')
 });
 
-app.post("/login", (req, res) => {
-    const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-    })
+APP.get("/login", (req, res) => {
+    console.log('am i working yet?')
+    // const code = req.body.code
+    // const spotifyApi = new SpotifyWebApi({
+    //     redirectUri: process.env.REDIRECT_URI,
+    //     clientId: process.env.CLIENT_ID,
+    //     clientSecret: process.env.CLIENT_SECRET,
+    // })
 
-    spotifyApi
-        .authorizationCodeGrant(code)
-        .then(data => {
-            res.json({
-                accessToken: data.body.access_token,
-                refreshToken: data.body.refresh_token,
-                expiresIn: data.body.expires_in,
-            })
-        })
-        .catch(err => {
-            res.sendStatus(400)
-        })
+    // spotifyApi
+    //     .authorizationCodeGrant(code)
+    //     .then(data => {
+    //         res.json({
+    //             accessToken: data.body.access_token,
+    //             refreshToken: data.body.refresh_token,
+    //             expiresIn: data.body.expires_in,
+    //         })
+    //     })
+    //     .catch(err => {
+    //         res.sendStatus(400)
+    //     })
 })
 
 //___________________
